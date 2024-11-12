e1cc6a2f3ffe99027555ec8de6f63ead6e410f05
AnderVzz <146988736+AnderVzz@users.noreply.github.com>
2024-11-12 16:06:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("holi")
+# Función que suma dos números
+def suma(a, b):
+    return a + b
+
+# Variables con dos números (puedes elegir cualquier valor)
+numero1 = 10
+numero2 = 15
+
+# Imprime el resultado de la suma de los dos números usando la función
+print("La suma de", numero1, "y", numero2, "es:", suma(numero1, numero2))
+
